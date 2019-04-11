2f1f87efdb6d96dd7c1e51421066b099159d3b4c
kuldeep semwal <kuldeepsemwal@kuldeeps-MacBook-Air.local>
2019-04-11 21:47:04 +0530

    first commit uploading initial files

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'admin',
+    loadChildren: './admin/admin.module#AdminModule'
+  },
+  {
+    path: 'user',
+    loadChildren: './user/user.module#UserModule'
+  },
+  {
+    path: 'vendor',
+    loadChildren: './vendor/vendor.module#VendorModule'
+  },
+  {
+    path: '',
+    redirectTo: '',
+    pathMatch: 'full'
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
